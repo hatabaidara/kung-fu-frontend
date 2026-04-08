85d75bdbb884a1b02000f01d35e70a64f66bcd18
hatabaidara <hatabaidara08@gmail.com>
2026-04-08 17:51:24 +0000

    fix: restore full application with App.tsx
    - Replace AppFinal with original App.tsx
    - Restore complete application functionality
    - Include all components and routing
    - Enable full Sport Gym Management features
    - Build complete application with all dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppFinal from './AppFinal.tsx'
+import App from './App.tsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppFinal />
+    <App />
   </React.StrictMode>,
 )
